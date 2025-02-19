54339fb885070022533da095c313cb69de79d625
root <azladji.sil@gmail.com>
2025-02-19 09:18:16 +0000

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(97 + i);
+	}
+	putchar(10);
+	return (0);
+}
