3047cadc0cdca706f01bc942f2cad30f8f7a9cbe
root <azladji.sil@gmail.com>
2025-02-21 14:29:22 +0000

    2-print_alphabet_x10.c

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,12 +1,2 @@
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_alphabet_x10();
-    return (0);
-}
+int _putchar(char c);
+void print_alphabet_x10(void);
