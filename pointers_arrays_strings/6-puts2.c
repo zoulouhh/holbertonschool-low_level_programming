c516860656887676a6e37bef169ca2e4dd56ba8c
root <azladji.sil@gmail.com>
2025-02-25 14:17:23 +0000

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,40 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * is_separator - Checks if a char is a separator
- * @c: Character
- * Return: 1 if separator, 0 otherwise
+ * puts2 - too long
+ * @str: String
  */
-int is_separator(char c)
+void puts2(char *str)
 {
 	int i;
-	char *separators = " \t\n,;.!?\"(){}";
 
-	for (i = 0; separators[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (c == separators[i])
-			return (1);
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	}
-	return (0);
-}
-
-/**
- * cap_string - Capitalizes all words of a string
- * @str: String
- * Return: Changed string
- */
-char *cap_string(char *str)
-{
-	int i = 0;
-	int should_cap = 1;
-
-	while (str[i] != '\0')
-	{
-		if (should_cap && str[i] >= 'a' && str[i] <= 'z')
-			str[i] = str[i] - 32;
-		should_cap = is_separator(str[i]);
-		i++;
-	}
-	return (str);
+	_putchar('\n');
 }
