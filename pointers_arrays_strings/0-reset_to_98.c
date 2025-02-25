a1d74d06a31a13a7851d192c244db4c4e3815fe7
root <azladji.sil@gmail.com>
2025-02-25 08:48:00 +0000

    0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,11 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * reset_to_98 - takes a pointer to an int and updates the value it points to 98
- * @n: pointer to an integer to be updated
- *
- * Return: Nothing (void function)
+ * reset_to_98 - Read the code
+ * @n: pointer of Num
  */
 void reset_to_98(int *n)
 {
-    *n = 98;  // Dereference the pointer and set the value it points to 98
+	*n = 98;
 }
