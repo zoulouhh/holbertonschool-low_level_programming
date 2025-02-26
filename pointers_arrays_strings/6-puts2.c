fbd15d99ffb2c6e8ece8f09849dac4ab7db16f29
root <azladji.sil@gmail.com>
2025-02-26 09:56:46 +0000

    main.h

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,1 @@
-#include "main.h"
-
-/**
- * puts2 - too long
- * @str: String
- */
-void puts2(char *str)
-{
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
-	}
-	_putchar('\n');
-
 
