0fa7e8b62d91159677cf6bb26e79548a3ff5621c
root <azladji.sil@gmail.com>
2025-02-26 08:47:46 +0000

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * puts2 - too long
+ * @str: String
  */
-int main(void)
+void puts2(char *str)
 {
-    char *str;
+	int i;
 
-    str = "0123456789";
-    puts2(str);
-    return (0);
-}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+
 
