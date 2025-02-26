2ddb9106c89d1f0ec064dceec990492970d5a8cf
root <azladji.sil@gmail.com>
2025-02-26 08:49:57 +0000

    main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-void _putchar(char c);
+void puts2(char *str);
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -6,4 +6,3 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s)
-void puts2(char *str);
