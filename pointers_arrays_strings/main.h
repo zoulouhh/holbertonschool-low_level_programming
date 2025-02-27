8c0601aef246458fe416f0134e71cb791fadf750
root <azladji.sil@gmail.com>
2025-02-27 14:27:14 +0000

    1-s

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,10 +1,1 @@
-void _putchar(char c);
-
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-int _atoi(char *s);
-void print_number(int n);
+char *_strncat(char *dest, char *src, int n);
