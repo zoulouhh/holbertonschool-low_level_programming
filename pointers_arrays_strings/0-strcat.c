c9efabe300cdb2d88bde4ab6801de4a4e8ed80e1
root <azladji.sil@gmail.com>
2025-02-28 14:35:52 +0000

    0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,16 +1,16 @@
 #include "main.h"
 char *_strcat(char *dest, char *src)
 {
-       	int BASSEM692 = 0;
-        int TEAMHLIB = 0;
+	int BASSEM692 = 0;
+	int TEAMHLIB = 0;
 
-        while (dest[BASSEM692] != '\0')
+	while (dest[BASSEM692] != '\0')
                 BASSEM692++;
-        while (src[TEAMHLIB] != '\0')
+	while (src[TEAMHLIB] != '\0')
         {
-                dest[BASSEM692+ TEAMHLIB] = src[TEAMHLIB];
-                TEAMHLIB++;
+		dest[BASSEM692+ TEAMHLIB] = src[TEAMHLIB];
+		TEAMHLIB++;
         }
 
-        return(dest);
+	return(dest);
 }
