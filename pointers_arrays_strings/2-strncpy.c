fa6a8747fa5b82a433732392632d9a10447d82fa
root <azladji.sil@gmail.com>
2025-03-02 22:06:08 +0000

    2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+nclude "main.h"
+
+/**
+ *  * _strncpy - Copies a string
+ *   * @dest: Destination string
+ *    * @src: Source string
+ *     * @n: bytes
+ *      * Return: Destination
+ *       */
+char *_strncpy(char *dest, char *src, int n)
+{
+		int i;
+
+			for (i = 0; i < n && src[i] != '\0'; i++)
+						dest[i] = src[i];
+				for (; i < n; i++)
+							dest[i] = '\0';
+					return (dest);
+}
