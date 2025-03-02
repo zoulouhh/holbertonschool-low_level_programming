76fd40adc568e97a7a23a5fab257d6bb1fa98ac3
root <azladji.sil@gmail.com>
2025-03-02 22:31:45 +0000

    7-leet.c

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+nclude "main.h"
+#include <stdio.h>
+
+/**
+ *  * leet - Encodes a string into leet
+ *   * @str: Input
+ *    * Return: Changed str
+ *     */
+char *leet(char *str)
+{
+		int i = 0, j;
+
+			char *original = "aAeEoOtTlL";
+				char *replace  = "4433007711";
+
+					while (str[i] != '\0')
+							{
+										for (j = 0; j <= 10; j++)
+													{
+																	if (str[i] == original[j])
+																						str[i] = replace[j];
+																			}
+												i++;
+													}
+						return (str);
+}
