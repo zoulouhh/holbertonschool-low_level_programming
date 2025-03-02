a58cadcbf2f27224449dc179f5b6d726b6c824b2
root <azladji.sil@gmail.com>
2025-03-02 22:22:59 +0000

    4-rev_array.c

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -20,4 +20,4 @@ void reverse_array(int *a, int n)
 																start++;
 																		end--;
 																			}
-}
+
