6f2669079a17de287c4e515f90ffd10318c9465a
root <azladji.sil@gmail.com>
2025-03-02 22:15:39 +0000

    3-strcmp.c

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *  * _strcmp - Compares two strings
+ *   * @s1: String 1
+ *    * @s2: String 2
+ *     * Return: RTFM :)
+ *      */
+int _strcmp(char *s1, char *s2)
+{
+		int i = 0;
+
+			while (s1[i] == s2[i])
+					{
+								if (s1[i] == '\0')
+												return (0);
+										i++;
+											}
+				return (s1[i] - s2[i]);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,2 +1,3 @@
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
