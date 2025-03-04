be316b43c8b12406cfbed5a61983d029d008aae5
root <azladji.sil@gmail.com>
2025-03-04 14:58:02 +0000

    3-strspn.c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -10,26 +10,26 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int count = 0;
-    int found;
-    char *a;
+	unsigned int count = 0;
+	int found;
+	char *a;
 
-    while (*s)
-    {
-        found = 0;
-        for (a = accept; *a; a++)
-        {
-            if (*s == *a)
-            {
-                found = 1;
-                break;
-            }
-        }
-        if (found == 0)
-            return count;
-        count++;
-        s++;
-    }
+	while (*s)
+	{
+	found = 0;
+	for (a = accept; *a; a++)
+	{
+	if (*s == *a)
+	{
+	found = 1;
+	break;
+	}
+	}
+	if (found == 0)
+	return (count);
+	count++;
+	s++;
+	}
 
-    return count;
+	return (count);
 }
