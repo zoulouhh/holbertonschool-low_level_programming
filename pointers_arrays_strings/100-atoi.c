3d49bc17725fc8fb4f818c145895672288526e1f
root <azladji.sil@gmail.com>
2025-03-06 02:47:07 +0000

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,12 +1,16 @@
+#include <limits.h>  // Include this header for INT_MAX and INT_MIN
+
 int _atoi(char *s)
 {
     int result = 0;
     int sign = 1;
     int i = 0;
 
+    // Skip leading whitespace
     while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))
         i++;
 
+    // Process '+' and '-' signs
     while (s[i] == '-' || s[i] == '+')
     {
         if (s[i] == '-')
@@ -14,11 +18,12 @@ int _atoi(char *s)
         i++;
     }
 
+    // Convert numeric part of string to integer
     while (s[i] >= '0' && s[i] <= '9')
     {
         if (result > INT_MAX / 10 || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
             return (sign == 1 ? INT_MAX : INT_MIN);
-        
+
         result = result * 10 + (s[i] - '0');
         i++;
     }
