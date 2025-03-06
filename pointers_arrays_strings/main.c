71d2b5fe1924298ed7a10fda61dad01056ab981d
root <azladji.sil@gmail.com>
2025-03-06 02:33:55 +0000

    main.c

--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -41,6 +41,5 @@ int main(void)
     simple_print_buffer(buffer, 98);
     _memset(buffer, 0x01, 95);
     printf("-------------------------------------------------\n");
-    simple_print_buffer(buffer, 98);    
-    return (0);
-}
+    simple_print_buffer(buffer, 98);
+    
