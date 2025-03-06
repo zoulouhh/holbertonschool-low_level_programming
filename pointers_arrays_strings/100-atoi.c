9db00b4e4f360af2b0877ec5bf394f9fb3024789
root <azladji.sil@gmail.com>
2025-03-06 02:46:05 +0000

    100-atoi.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+int _atoi(char *s)
+{
+    int result = 0;
+    int sign = 1;
+    int i = 0;
+
+    while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))
+        i++;
+
+    while (s[i] == '-' || s[i] == '+')
+    {
+        if (s[i] == '-')
+            sign *= -1;
+        i++;
+    }
+
+    while (s[i] >= '0' && s[i] <= '9')
+    {
+        if (result > INT_MAX / 10 || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
+            return (sign == 1 ? INT_MAX : INT_MIN);
+        
+        result = result * 10 + (s[i] - '0');
+        i++;
+    }
+
+    return result * sign;
+}
