b8c84d2304f910b8d6694e0abef0b4208f9616a2
root <azladji.sil@gmail.com>
2025-03-06 02:16:16 +0000

    1-strncat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -5,22 +5,21 @@
 /**
  *  * _strncat - Appends the `src` string to the `dest` string
  *   * @dest: Destination string
- *    * @src: Source string
- *     * @n: Bytes amount
- *      * Return: Destination pointer
- *       */
+ *   * @src: Source string
+ *   * @n: Bytes amount
+ *   * Return: Destination pointer
+ *   */
 char *_strncat(char *dest, char *src, int n)
 {
 		int dI = 0, sI = 0;
 
-			while (dest[dI] != '\0')
-						dI++;
+		while (dest[dI] != '\0')
+			dI++;
 
-				while (src[sI] != '\0' && n > sI)
-						{
-									dest[dI + sI] = src[sI];
-											sI++;
-												}
+		while (src[sI] != '\0' && n > sI)
+		{ 
+			dest[dI + sI] = src[sI];													sI++;
+						}
 
-					return (dest);
+		return (dest);
 }
