1d7b8a1e60127a62b7489f3ef4d03e3871678e9e
root <azladji.sil@gmail.com>
2025-03-06 02:21:52 +0000

    7-print_chessboard.c

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -2,30 +2,19 @@
 #include <stdio.h>
 
 /**
- * _strstr - Locates a substring
- * @haystack: ?
- * @needle: ?
- * Return: First occurrence of the substring needle in the string haystack
- */
-char *_strstr(char *haystack, char *needle)
+ *  * print_chessboard - Prints a chess board
+ *   * @a: Board
+ *    */
+void print_chessboard(char (*a)[8])
 {
-	int i;
+		int i, j;
 
-	if (*needle == '\0')
-		return (haystack);
-
-	while (*haystack != '\0')
-	{
-		i = 0;
-
-		while (needle[i] != '\0' && haystack[i] != '\0' &&
-				needle[i] == haystack[i])
-			i++;
-
-		if (needle[i] == '\0')
-			return (haystack);
-
-		haystack++;
-	}
-	return (NULL);
+			for (i = 0; i < 8; i++)
+					{
+								for (j = 0; j < 8; j++)
+											{
+															_putchar(a[i][j]);
+																	}
+										_putchar('\n');
+											}
 }
