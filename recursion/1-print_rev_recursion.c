a1fa7d7e40bddb84d02b02ef786e3f4d4744e512
root <azladji.sil@gmail.com>
2025-03-07 03:13:58 +0000

    1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
-#include <stdio.h>
+
+/* Function prototype for _putchar */
+int _putchar(char c);
 
 /**
- * _print_rev_recursion - Prints a reversed string in recursive
- * @s: String to be printed
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: The string to be printed
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		return;
-	}
+    if (*s == '\0') /* Base case: End of the string */
+        return;
 
-	_print_rev_recursion(s + 1);
-	_putchar(*s);
+    _print_rev_recursion(s + 1); /* Recursive call with the next character */
+    _putchar(*s); /* Print the current character after the recursive call */
 }
