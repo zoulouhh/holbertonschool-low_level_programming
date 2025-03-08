0795eaf76968d6049d3fd189958706966b220b15
root <azladji.sil@gmail.com>
2025-03-08 03:33:02 +0000

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -9,11 +9,11 @@
  */
 int _sqrt_helper(int n, int guess)
 {
-    if (guess * guess > n)
-        return (-1);
-    if (guess * guess == n)
-        return (guess);
-    return (_sqrt_helper(n, guess + 1));
+	if (guess * guess > n)
+	return (-1);
+	if (guess * guess == n)
+	return (guess);
+	return (_sqrt_helper(n, guess + 1));
 }
 
 /**
@@ -24,7 +24,7 @@ int _sqrt_helper(int n, int guess)
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return (-1);
-    return (_sqrt_helper(n, 0));
+	if (n < 0)
+	return (-1);
+	return (_sqrt_helper(n, 0));
 }
