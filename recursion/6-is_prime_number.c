f1075fa9ba09468fce2045f926fb04b29ab73003
root <azladji.sil@gmail.com>
2025-03-08 03:36:50 +0000

    6-is_prime_number.c

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -9,11 +9,11 @@
  */
 int check_prime(int n, int i)
 {
-    if (i * i > n)
-        return (1);
-    if (n % i == 0)
-        return (0);
-    return (check_prime(n, i + 1));
+	if (i * i > n)
+	return (1);
+	if (n % i == 0)
+	return (0);
+	return (check_prime(n, i + 1));
 }
 
 /**
@@ -24,7 +24,7 @@ int check_prime(int n, int i)
  */
 int is_prime_number(int n)
 {
-    if (n <= 1)
-        return (0);
-    return (check_prime(n, 2));
+	if (n <= 1)
+	return (0);
+	return (check_prime(n, 2));
 }
