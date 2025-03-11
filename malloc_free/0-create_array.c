4633ef001560926990b8b70c331a603e2d8dcfcd
root <azladji.sil@gmail.com>
2025-03-11 09:46:09 +0000

    0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -6,14 +6,14 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-	return NULL;
+	return (NULL);
 
 	array = malloc(sizeof(char) * size);
 	if (array == NULL)
-	return NULL;
+	return (NULL);
 
 	for (i = 0; i < size; i++)
 	array[i] = c;
 
-	return array;
+	return (arra)y;
 }
