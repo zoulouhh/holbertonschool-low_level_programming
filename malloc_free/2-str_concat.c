46933d1ff3db684aac8a09ff3a37119a220e768c
root <azladji.si@gmail.com>
2025-03-13 08:48:46 +0000

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,37 +1,45 @@
 #include <stdlib.h>
 
+/**
+ * str_concat - Concatenates two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+ * Return: A pointer to the newly allocated space in memory
+ *         containing the concatenated string, or NULL on failure.
+ */
 char *str_concat(char *s1, char *s2)
 {
-    char *concat;
-    unsigned int len1 = 0, len2 = 0, i, j;
+	char *concat;
+	unsigned int len1 = 0, len2 = 0, i, j;
 
-    /* Treat NULL as empty string */
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	/* Treat NULL as empty string */
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    /* Get lengths of strings */
-    while (s1[len1])
-        len1++;
-    while (s2[len2])
-        len2++;
+	/* Get lengths of strings */
+	while (s1[len1])
+		len1++;
+	while (s2[len2])
+		len2++;
 
-    /* Allocate memory for concatenated string */
-    concat = malloc(sizeof(char) * (len1 + len2 + 1));
-    if (concat == NULL)
-        return NULL;
+	/* Allocate memory for concatenated string */
+	concat = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (concat == NULL)
+		return (NULL);
 
-    /* Copy s1 into new string */
-    for (i = 0; i < len1; i++)
-        concat[i] = s1[i];
+	/* Copy s1 into new string */
+	for (i = 0; i < len1; i++)
+		concat[i] = s1[i];
 
-    /* Append s2 to new string */
-    for (j = 0; j < len2; j++, i++)
-        concat[i] = s2[j];
+	/* Append s2 to new string */
+	for (j = 0; j < len2; j++, i++)
+		concat[i] = s2[j];
 
-    /* Add null terminator */
-    concat[i] = '\0';
+	/* Add null terminator */
+	concat[i] = '\0';
 
-    return concat;
+	return (concat);
 }                                               
