4b0949a3be52d8d9d6303318e6bc3646e320ae27
root <azladji.si@gmail.com>
2025-03-13 01:07:08 +0000

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -32,7 +32,7 @@ char *str_concat(char *s1, char *s2)
 
     return (concat);
 }
-                                                                                                                                                                                                                                                                        
+~                                                                                                                                                                                                                                                                        
 ~                                                                                                                                                                                                                                                                        
 ~                                                                                                                                                                                                                                                                        
 ~                                                                                                                                                                                                                                                                        
