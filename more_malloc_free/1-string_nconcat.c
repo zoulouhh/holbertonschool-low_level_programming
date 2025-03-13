a72fc93752396fb0400bce0a96c296aa13cc5601
root <azladji.si@gmail.com>
2025-03-13 10:18:10 +0000

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * string_nconcat - Concatenates two strings.
@@ -15,11 +14,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
     char *concat;
     unsigned int len1 = 0, len2 = 0, i, j;
 
-    /* Treat NULL strings as empty strings */
-    if (s1 != NULL)
-        len1 = strlen(s1);
-    if (s2 != NULL)
-        len2 = strlen(s2);
+    /* Calculate lengths of s1 and s2 */
+    if (s1)
+        for (len1 = 0; s1[len1]; len1++);
+    if (s2)
+        for (len2 = 0; s2[len2]; len2++);
 
     /* Use the full length of s2 if n is greater than or equal to len2 */
     if (n >= len2)
