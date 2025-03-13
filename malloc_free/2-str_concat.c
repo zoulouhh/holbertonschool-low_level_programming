ab46ddc8b83c0344cc01ec3dd1f6e38220c99b7b
root <azladji.si@gmail.com>
2025-03-13 08:44:06 +0000

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,29 +1,37 @@
 #include <stdlib.h>
-#include <string.h>
 
 char *str_concat(char *s1, char *s2)
 {
     char *concat;
-    int len1, len2;
+    unsigned int len1 = 0, len2 = 0, i, j;
 
-    // Treat NULL as empty string
+    /* Treat NULL as empty string */
     if (s1 == NULL)
         s1 = "";
     if (s2 == NULL)
         s2 = "";
 
-    // Get lengths of strings
-    len1 = strlen(s1);
-    len2 = strlen(s2);
+    /* Get lengths of strings */
+    while (s1[len1])
+        len1++;
+    while (s2[len2])
+        len2++;
 
-    // Allocate memory for concatenated string
+    /* Allocate memory for concatenated string */
     concat = malloc(sizeof(char) * (len1 + len2 + 1));
     if (concat == NULL)
         return NULL;
 
-    // Copy s1 and s2 into new string
-    strcpy(concat, s1);
-    strcat(concat, s2);
+    /* Copy s1 into new string */
+    for (i = 0; i < len1; i++)
+        concat[i] = s1[i];
+
+    /* Append s2 to new string */
+    for (j = 0; j < len2; j++, i++)
+        concat[i] = s2[j];
+
+    /* Add null terminator */
+    concat[i] = '\0';
 
     return concat;
 }                                               
