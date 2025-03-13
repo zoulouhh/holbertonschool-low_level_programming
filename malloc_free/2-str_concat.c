1bfabffca906df273e6dd4cfd31c857d38e90432
root <azladji.si@gmail.com>
2025-03-13 01:08:15 +0000

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,11 +14,15 @@ char *str_concat(char *s1, char *s2)
     int i, j, len1 = 0, len2 = 0;
 
     if (s1 != NULL)
+    {
         while (s1[len1])
             len1++;
+    }
     if (s2 != NULL)
+    {
         while (s2[len2])
             len2++;
+    }
 
     concat = malloc(sizeof(char) * (len1 + len2 + 1));
     if (concat == NULL)
@@ -31,8 +35,7 @@ char *str_concat(char *s1, char *s2)
     concat[i + j] = '\0';
 
     return (concat);
-}
-~                                                                                                                                                                                                                                                                        
+}                                                                                                                                                                                                                                                                        
 ~                                                                                                                                                                                                                                                                        
 ~                                                                                                                                                                                                                                                                        
 ~                                                                                                                                                                                                                                                                        
