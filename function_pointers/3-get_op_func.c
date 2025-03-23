000a59a446c3b0a37f743e1efde4af06207d50bf
root <azladji.sil@gmail.com>
2025-03-23 04:56:59 +0000

    3-get_op_func.c

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,29 +1,28 @@
 #include "3-calc.h"
 
 /**
- * get_op_func - Sélectionne la fonction correcte en fonction de l'opérateur
- * @s: L'opérateur passé en argument
- * Return: Pointeur vers la fonction correspondant à l'opérateur, NULL sinon
+ * get_op_func - sélectionne la bonne fonction d'opération
+ * @s: opérateur fourni en argument
+ * Return: pointeur vers la fonction correspondante ou NULL si invalide
  */
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-    while (ops[i].op != NULL)
-    {
-        if (s[0] == ops[i].op[0] && s[1] == '\0') /* Vérifie que l'opérateur est un seul caractère */
-            return (ops[i].f);
-        i++;
-    }
-
-    return (NULL);
+	while (ops[i].op)
+	{
+		if (*s == *(ops[i].op) && s[1] == '\0')
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
 
