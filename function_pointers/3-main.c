69326dbac50a618f50e06ba729af8ed37a1d0591
root <azladji.sil@gmail.com>
2025-03-23 04:58:20 +0000

    3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,47 +1,35 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "3-calc.h"
 
 /**
- * main - Entry point for the calculator program.
- * @argc: Number of arguments passed to the program.
- * @argv: Array of arguments passed to the program.
- *
- * Return: 0 on success, or exit with specific error codes on failure:
- *         98 if wrong number of arguments,
- *         99 if invalid operator,
- *         100 if division/modulo by zero is attempted.
+ * main - programme principal qui effectue une opération
+ * @argc: nombre d'arguments
+ * @argv: tableau des arguments
+ * Return: 0 en cas de succès, un code d'erreur sinon
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-    int (*operation)(int, int);
+	int num1, num2, result;
+	int (*operation)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
-    operation = get_op_func(argv[2]);
-    if (!operation)
-    {
-        printf("Error\n");
-        exit(99);
-    }
+	operation = get_op_func(argv[2]);
+	if (!operation)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
+	result = operation(num1, num2);
+	printf("%d\n", result);
 
-    result = operation(num1, num2);
-    printf("%d\n", result);
-
-    return (0);
+	return (0);
 }
 
