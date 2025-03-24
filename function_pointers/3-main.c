5d741df528ba38ecf39ee7bdf8b5c80448a1bd05
root <azladji.sil@gmail.com>
2025-03-24 08:22:06 +0000

    3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,35 +1,42 @@
 #include "3-calc.h"
 
 /**
- * main - programme principal qui effectue une opération
- * @argc: nombre d'arguments
- * @argv: tableau des arguments
- * Return: 0 en cas de succès, un code d'erreur sinon
+ * main - performs simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 on success, 98, 99, or 100 on error
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
-	int (*operation)(int, int);
+    int num1, num2, result;
+    int (*operation)(int, int);
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[3]);
 
-	operation = get_op_func(argv[2]);
-	if (!operation)
-	{
-		printf("Error\n");
-		exit(99);
-	}
+    operation = get_op_func(argv[2]);
 
-	result = operation(num1, num2);
-	printf("%d\n", result);
+    if (operation == NULL)
+    {
+        printf("Error\n");
+        exit(99);
+    }
 
-	return (0);
+    if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+    {
+        printf("Error\n");
+        exit(100);
+    }
+
+    result = operation(num1, num2);
+    printf("%d\n", result);
+
+    return (0);
 }
 
