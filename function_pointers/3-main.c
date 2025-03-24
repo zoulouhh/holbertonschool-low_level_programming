3d9aa334848db436a8079b3c7627c6bff9a28186
root <azladji.sil@gmail.com>
2025-03-24 08:28:17 +0000

    3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -8,35 +8,34 @@
  */
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-    int (*operation)(int, int);
+	int num1, num2, result;
+	int (*operation)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 4)
+	{
+	printf("Error\n");
+	exit(98);
+	}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
-    operation = get_op_func(argv[2]);
+	operation = get_op_func(argv[2]);
 
-    if (operation == NULL)
-    {
+	if (operation == NULL)
+	{
         printf("Error\n");
         exit(99);
-    }
+	}
 
-    if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	{
+	printf("Error\n");
+	exit(100);
+	}
 
-    result = operation(num1, num2);
-    printf("%d\n", result);
+	result = operation(num1, num2);
+	printf("%d\n", result);
 
-    return (0);
+	return (0);
 }
-
