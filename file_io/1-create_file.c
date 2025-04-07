77a2d99c5ac7e8963c6c2135cd8ae88dca5f1540
root <azladji.sil@gmail.com>
2025-04-07 12:19:04 +0000

    1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,43 +1,33 @@
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <stdlib.h>
-
+#include "main.h"
 /**
- * create_file - Creates a file and writes text_content to it.
- * @filename: The name of the file to create.
- * @text_content: A NULL-terminated string to write to the file.
- *
- * Return: 1 on success, -1 on failure.
- */
+* create_file - creates file withe given content
+* @filename: name of the file to create
+* @text_content: a NULL terminated string to write to the file
+* Return: 1 succes, -1 fail
+*/
 int create_file(const char *filename, char *text_content)
 {
-    int fd, write_status;
-    ssize_t len = 0;
+    int fd, w, len = 0;
 
     if (filename == NULL)
         return (-1);
 
-    /* Open the file with O_CREAT | O_WRONLY | O_TRUNC flags */
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
     if (fd == -1)
         return (-1);
 
-    /* If text_content is not NULL, calculate its length and write it */
     if (text_content != NULL)
     {
-        while (text_content[len] != '\0')
+        while (text_content[len])
             len++;
-        write_status = write(fd, text_content, len);
-        if (write_status == -1)
+
+        w = write(fd, text_content, len);
+        if (w == -1 || w != len)
         {
             close(fd);
             return (-1);
         }
     }
-
-    /* Close the file descriptor */
     close(fd);
     return (1);
-}
-
+}     
